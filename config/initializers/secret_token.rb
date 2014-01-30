f8bbd75fb9725e2717f76b70e952d4cf0c8b384e
Uday <uday88k@gmail.com>
2014-01-30 13:18:48 -0500

    first version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+D3sample::Application.config.secret_key_base = '2abe6ad53fbf04fb479cd9ab0d35e940d9e09954e2119806c1195b9dcdcf50aeb0142980ecc5b8fd1b66b62594b3a6a97f0a8e67a1e393c98929ee6b2261c17d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+D3sample::Application.config.session_store :cookie_store, key: '_d3sample_session'
